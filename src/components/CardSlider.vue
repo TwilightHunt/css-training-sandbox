49d7eab52427298cffa9d202cf145b88480d0398
Vera Lavrentyeva <55912590+TwilightHunt@users.noreply.github.com>
2023-04-27 15:05:44 +0300

    build: card slider

--- src/components/CardSlider.vue
+++ src/components/CardSlider.vue
@@ -1,28 +1,87 @@
 <template>
-  <div class="card-slider">
-    <Card v-for="card in cards" :key="card.id" :data="card" />
+  <div class="card-slider m-10">
+    <div v-if="currentOffset < 0" class="card-slider__chevron-left py-6 px-2" @click="slideTo(1)">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-8 h-8 stroke-white">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M15.75 19.5L8.25 12l7.5-7.5" />
+      </svg>
+    </div>
+    <div ref="slider" class="card-slider__body">
+      <Card v-for="card in cards" :key="card.id" :data="card" />
+    </div>
+    <div
+      v-if="currentOffset > -300"
+      class="card-slider__chevron-right py-6 px-2"
+      @click="slideTo(-1)">
+      <svg
+        xmlns="http://www.w3.org/2000/svg"
+        fill="none"
+        viewBox="0 0 24 24"
+        stroke-width="1.5"
+        stroke="currentColor"
+        class="w-8 h-8 stroke-white">
+        <path stroke-linecap="round" stroke-linejoin="round" d="M8.25 4.5l7.5 7.5-7.5 7.5" />
+      </svg>
+    </div>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { fetchItems } from "../composables/useFetch.ts";
 import Card from "./Card.vue";
 import { ref, onMounted } from "vue";
 
 const cards = ref();
+const slider = ref(null);
+let currentOffset: number = ref(0);
+const cardAmount: number = 6;
+
+const clamp = (num, min, max) => Math.min(Math.max(num, min), max);
 
 onMounted(async () => {
   cards.value = await fetchItems();
-  console.log(cards.value);
 });
+
+const slideTo = (delta: number) => {
+  const offset = currentOffset.value + 100 * delta;
+  currentOffset.value = clamp(offset, -300, 0);
+  slider.value.style.transform = `translate(${currentOffset.value}%, 0)`;
+};
 </script>
 
 <style lang="scss" scoped>
 .card-slider {
-  display: grid;
-  align-items: baseline;
-  grid-auto-flow: column;
-  grid-auto-columns: minmax(200px, auto);
   overflow-x: hidden;
+  position: relative;
+}
+.card-slider__body {
+  display: grid;
+  grid-auto-flow: column;
+  grid-auto-columns: minmax(16.666%, auto);
+  transition: transform 0.3s ease-in-out;
+}
+.card-slider__chevron-left {
+  position: absolute;
+  background-color: rgba($color: #000000, $alpha: 0.5);
+  top: 50%;
+  left: 0;
+  transform: translateY(-50%);
+  opacity: 0;
+  transition: all 0.2s ease-out;
+  cursor: pointer;
+  z-index: 5;
+  &:hover {
+    opacity: 1;
+  }
+}
+.card-slider__chevron-right {
+  @extend .card-slider__chevron-left;
+  right: 0;
+  left: unset;
 }
 </style>
